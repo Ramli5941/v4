c3986f83ba549eebec362272851cba752b052523
Ramli5941 <ramlimuji5941@gmail.com>
2024-08-25 03:43:19 +0800

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -272,13 +272,19 @@ netfilter-persistent reload
 cd /usr/bin
 # menu
 wget -O menu "https://raw.githubusercontent.com/Ramli5941/v4/main/menu/menu.sh"
-wget -O menu-trial "https://raw.githubusercontent.com/Fikripps/Ver3/main/menu/menu-trial.sh"
 wget -O menu-vmess "https://raw.githubusercontent.com/Ramli5941/v4/main/menu/menu-vmess.sh"
-wget -O menu-vless "https://raw.githubusercontent.com/Ramli5941/v4/main/menu-vless.sh"
+wget -O menu-vless "https://raw.githubusercontent.com/Ramli5941/v4/main/menu/menu-vless.sh"
 wget -O running "https://raw.githubusercontent.com/Ramli5941/v4/main/menu/running.sh"
-wget -O clearcache "https://raw.githubusercontent.com/Fikripps/Ver3/main/menu/clearcache.sh"
+wget -O about "https://raw.githubusercontent.com/Ramli5941/v4/main/menu/about.sh"
+wget -O clearcache "https://raw.githubusercontent.com/Ramli5941/v4/main/menu/clearcache.sh"
 wget -O menu-trgo "https://raw.githubusercontent.com/Ramli5941/v4/main/menu/menu-trgo.sh"
 wget -O menu-trojan "https://raw.githubusercontent.com/Ramli5941/v4/main/menu/menu-trojan.sh"
+wget -O trialvless "https://raw.githubusercontent.com/Ramli5941/v4/main/xray/trialvless.sh"
+wget -O add-vless "https://raw.githubusercontent.com/Ramli5941/v4/main/xray/add-vless.sh"
+wget -O renew-vless "https://raw.githubusercontent.com/Ramli5941/v4/main/xray/renew-vless.sh"
+wget -O cek-vless "https://raw.githubusercontent.com/Ramli5941/v4/main/xray/cek-vless.sh"
+wget -0 add-vless2 "https://raw.githubusercontent.com/Ramli5941/v4/main/xray/add-vless2.sh"
+wget -0 del-vless "https://raw.githubusercontent.com/Ramli5941/v4/main/xray/del-vless.sh"
 
 # menu ssh ovpn
 wget -O menu-sshh "https://raw.githubusercontent.com/Ramli5941/v4/main/menu/menu-sshh.sh"
@@ -292,40 +298,36 @@ wget -O delete "https://raw.githubusercontent.com/Ramli5941/v4/main/ssh/delete.s
 wget -O autokilll "https://raw.githubusercontent.com/Ramli5941/v4/main/ssh/autokilll.sh"
 wget -O ceklim "https://raw.githubusercontent.com/Ramli5941/v4/main/ssh/ceklim.sh"
 wget -O tendang "https://raw.githubusercontent.com/Ramli5941/v4/main/ssh/tendang.sh"
-wget -O user-lock "https://raw.githubusercontent.com/Ramli5941/v4/main/ssh/user-lock.sh"
-wget -O user-unlock "https://raw.githubusercontent.com/Ramli5941/v4/main/ssh/user-unlock.sh"
 
 # menu system
 wget -O menu-set "https://raw.githubusercontent.com/Ramli5941/v4/main/menu/menu-set.sh"
 wget -O menu-domain "https://raw.githubusercontent.com/Ramli5941/v4/main/menu/menu-domain.sh"
 wget -O add-host "https://raw.githubusercontent.com/Ramli5941/v4/main/ssh/add-host.sh"
-wget -O port-change "https://raw.githubusercontent.com/Ramli5941/v4/main/port/port-change.sh"
 wget -O certv2ray "https://raw.githubusercontent.com/Ramli5941/v4/main/xray/certv2ray.sh"
 wget -O menu-webmin "https://raw.githubusercontent.com/Ramli5941/v4/main/menu/menu-webmin.sh"
 wget -O speedtest "https://raw.githubusercontent.com/Ramli5941/v4/main/ssh/speedtest_cli.py"
-wget -O about "https://raw.githubusercontent.com/Fikripps/Ver3/main/menu/about.sh"
+wget -O about "https://raw.githubusercontent.com/Ramli5941/v4/main/menu/about.sh"
 wget -O auto-reboot "https://raw.githubusercontent.com/Ramli5941/v4/main/menu/auto-reboot.sh"
 wget -O restarts "https://raw.githubusercontent.com/Ramli5941/v4/main/menu/restarts.sh"
 wget -O bw "https://raw.githubusercontent.com/Ramli5941/v4/main/menu/bw.sh"
 
-
 # change port
-wget -O port-ssl "https://raw.githubusercontent.com/Ramli5941/v4/main/port/port-ssl.sh"
-wget -O port-ovpn "https://raw.githubusercontent.com/Ramli5941/v4/main/port/port-ovpn.sh"
+
 
 
 wget -O xp "https://raw.githubusercontent.com/Ramli5941/v4/main/ssh/xp.sh"
-wget -O acs-set "https://raw.githubusercontent.com/Ramli5941/v4/main/acs-set.sh"
 
 wget -O sshws "https://raw.githubusercontent.com/Ramli5941/v4/main/ssh/sshws.sh"
 wget -O status "https://raw.githubusercontent.com/Ramli5941/v4/main/status.sh"
-wget -O menu-backup "https://raw.githubusercontent.com/Ramli5941/v4/main/backup/menu-backup.sh"
-wget -O backup "https://raw.githubusercontent.com/Ramli5941/v4/main/backup/backup.sh"
-wget -O restore "https://raw.githubusercontent.com/Ramli5941/v4/main/backup/restore.sh"
-wget -O bot "https://raw.githubusercontent.com/Ramli5941/v4/main/bot.sh"
-
+wget -O /usr/bin/menu "https://raw.githubusercontent.com/Ramli5941/v4/main/menu/menu.sh" && chmod +x /usr/bin/menu
+wget -0 menu-backup "https://raw.githubusercontent.com/Ramli5941/v4/main/menu/menu-backup.sh"
+wget -O /usr/bin/backup "https://raw.githubusercontent.com/Ramli5941/v4/main/backup/backup.sh" && chmod +x /usr/bin/backup
+wget -O /usr/bin/restore "https://raw.githubusercontent.com/Ramli5941/v4/main/backup/restore.sh" && chmod +x /usr/bin/restore
 wget -O jam "https://raw.githubusercontent.com/Ramli5941/v4/main/jam.sh"
-wget -O up "https://raw.githubusercontent.com/Ramli5941/v4/main/up.sh"
+wget -O quota "https://raw.githubusercontent.com/Ramli5941/v4/main/quota"
+wget -O limit "https://raw.githubusercontent.com/Ramli5941/v4/main/limit.sh"
+wget -O limit-ip "https://raw.githubusercontent.com/Ramli5941/v4/main/limit-ip"
+wget -O udp " https://raw.githubusercontent.com/Ramli5941/v4/main/udp/udp.sh"
 
 chmod +x menu
 chmod +x menu-trial
@@ -363,9 +365,6 @@ chmod +x auto-reboot
 chmod +x restarts
 chmod +x bw
 
-chmod +x port-ssl
-chmod +x port-ovpn
-
 chmod +x xp
 chmod +x acs-set
 chmod +x sshws
@@ -374,6 +373,10 @@ chmod +x menu-backup
 chmod +x backup
 chmod +x restore
 chmod +x jam
+chmod +x quota
+chmod +x limit
+chmod +x limit-ip
+chmod +x udp
 cd
 
 
