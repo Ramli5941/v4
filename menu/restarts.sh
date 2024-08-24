8dd9b5e05736885845bd4f48f66d018d736bdddc
Ramli5941 <ramlimuji5941@gmail.com>
2024-08-25 04:21:50 +0800

    Update restarts.sh

--- menu/restarts.sh
+++ menu/restarts.sh
@@ -40,7 +40,6 @@ case $Restart in
                 /etc/init.d/ssh restart
                 /etc/init.d/dropbear restart
                 /etc/init.d/stunnel4 restart
-                /etc/init.d/openvpn restart
                 /etc/init.d/fail2ban restart
                 /etc/init.d/cron restart
                 /etc/init.d/nginx restart
@@ -51,7 +50,6 @@ case $Restart in
                 echo -e "[ \033[1;36mOK\033[0m ] Restarting badvpn (via systemctl) "
                 screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500
                 echo -e "[ \033[1;36mOK\033[0m ] Restart Websocket (via systemctl) "
-                systemctl restart sshws.service
                 systemctl restart ws-dropbear.service
                 systemctl restart ws-stunnel.service
                 echo -e "[ \033[1;36mOK\033[0m ] Restart Trojan Go (via systemctl) "
@@ -212,7 +210,6 @@ case $Restart in
                 sleep 1
                 echo -e "[ \033[32mok\033[0m ] Restarting websocket Service (via systemctl) "
                 sleep 0.5
-                systemctl restart sshws.service
                 systemctl restart ws-dropbear.service
                 systemctl restart ws-stunnel.service
                 sleep 0.5
