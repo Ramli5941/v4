fc3fb87040bdc07133cf15336fd39fb560934541
Ramli5941 <ramlimuji5941@gmail.com>
2025-02-08 00:59:33 +0800

    Update menu-set.sh

--- menu/menu-set.sh
+++ menu/menu-set.sh
@@ -14,15 +14,14 @@ echo -e "        ═════════════════════
 echo -e "                     ${red_background}SYSTEM MENU${NC}"
 echo -e "        ═══════════════════════════════════════════"
 echo -e "         ${white}[•1]${NC} ${cyan}ganti Domain vps\033[0m"
-echo -e "         ${white}[•2]${NC} ${cyan}ganti Port\033[0m"
-echo -e "         ${white}[•3]${NC} ${cyan}ganti banner\033[0m"
-echo -e "         ${white}[•4]${NC} ${cyan}Restart service\033[0m"
-echo -e "         ${white}[•5]${NC} ${cyan}Cek bandwith\033[0m"
-echo -e "         ${white}[•6]${NC} ${cyan}Atur waktu riset\033[0m"
-echo -e "         ${white}[•7]${NC} ${cyan}Speedtest vps\033[0m"
-echo -e "         ${white}[•8]${NC} ${cyan}Backup-Bot-telegram\033[0m"
-echo -e "         ${white}[•9]${NC} ${cyan}NS-Baru/SlowDns\033[0m"
-echo -e "         ${white}[10]${NC} ${cyan}Helium\033[0m"
+echo -e "         ${white}[•2]${NC} ${cyan}ganti banner\033[0m"
+echo -e "         ${white}[•3]${NC} ${cyan}Restart service\033[0m"
+echo -e "         ${white}[•4]${NC} ${cyan}Cek bandwith\033[0m"
+echo -e "         ${white}[•5]${NC} ${cyan}Atur waktu riset\033[0m"
+echo -e "         ${white}[•6]${NC} ${cyan}Speedtest vps\033[0m"
+echo -e "         ${white}[•7]${NC} ${cyan}Backup-Bot-telegram\033[0m"
+echo -e "         ${white}[•8]${NC} ${cyan}NS-Baru/SlowDns\033[0m"
+echo -e "         ${white}[•9]${NC} ${cyan}Helium\033[0m"
 echo -e "         ${yelow}[•0]${NC} ${yelow}Back To Menu\033[0m"
 echo -e "         ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 echo -e "${yellow}"
@@ -30,15 +29,14 @@ read -p " >>>   " opt
 echo -e ""
 case $opt in
 1) clear ; menu-domain ; exit ;;
-2) clear ; port-change ; exit ;;
-3) clear ; nano /etc/issue.net ; exit ;;  #ssh-vpn banner.conf
-4) clear ; restarts ; exit ;;
-5) clear ; bw ; exit ;;
-6) clear ; auto-reboot ; exit ;;
-7) clear ; speedtest ; exit ;;
-8) clear ; autobackup ;;
-9) clear ; wget https://raw.githubusercontent.com/KhaiVpn767/SlowDnsV1/main/dns2.sh && chmod +x dns2.sh && ./dns2.sh ;;
-10) clear ; m-helium;;
+2) clear ; nano /etc/issue.net ; exit ;;  #ssh-vpn banner.conf
+3) clear ; restarts ; exit ;;
+4) clear ; bw ; exit ;;
+5) clear ; auto-reboot ; exit ;;
+6) clear ; speedtest ; exit ;;
+7) clear ; autobackup ;;
+8) clear ; wget https://raw.githubusercontent.com/KhaiVpn767/SlowDnsV1/main/dns2.sh && chmod +x dns2.sh && ./dns2.sh ;;
+9) clear ; m-helium;;
 0) clear ; menu ; exit ;;
 x) exit ;;
 *) echo -e "" ; echo "salah tekan" ; sleep 1 ; menu-set ;;
